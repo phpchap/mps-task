b60675394695bb9e1855a385b477f04213f2d9eb
Your Name <you@example.com>
2016-09-20 09:04:25 +0100

    changed done to completed in the UI

--- web/resources/views/tasks/inProgress.blade.php
+++ web/resources/views/tasks/inProgress.blade.php
@@ -5,7 +5,7 @@
             <h2><?php echo $task['title']; ?></h2>
             <p><?php echo $task['description']; ?></p>
             <p>(<a href="{{ route('move_to_new_task', ['id' => $task['id']]) }}">new</a> /
-                <a href="{{ route('move_to_complete_task', ['id' => $task['id']]) }}">done</a> /
+                <a href="{{ route('move_to_complete_task', ['id' => $task['id']]) }}">completed</a> /
                 <a href="{{ route('delete_task', ['id' => $task['id']]) }}">delete</a>)
             </p>
         </li>
